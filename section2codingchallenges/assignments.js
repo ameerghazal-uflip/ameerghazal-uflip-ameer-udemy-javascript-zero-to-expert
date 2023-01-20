266bc75f7b1703e47d62b02ce56a8fd765ae005b
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-20 17:15:02 -0600

    challenge #2

--- section2codingchallenges/assignments.js
+++ section2codingchallenges/assignments.js
@@ -1,7 +1,17 @@
-// String and Template Literals
-
-const description2 = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(description2);
+// Coding Challenge #2
 
 
+if (markBMI > johnBMI) {
+    console.log("Mark's BMI is higher than John's!");
+}
+else {
+    console.log("John's BMI is hihger than Mark's!");
+}
 
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI /(${markBMI}) is higher than John's (${johnBMI})!`);
+}
+else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`);
+}
