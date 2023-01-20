676664e784821fe469d3f925f71d56cbb5c44afc
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-20 17:21:11 -0600

    update

--- section2codingchallenges/assignments.js
+++ section2codingchallenges/assignments.js
@@ -1,3 +1,14 @@
-// Coding Challenge #2
+// Taking Decisions: if/else statments
 
 
+let language = "English";
+const country = "United States";
+const continent = "North America";
+let population = 331.45;
+
+if (population > 33) {
+    console.log(`${country}'s population is above average`);
+}
+else {
+    console.log(`${country}'s population is ${33 - population} million below average`);
+}
