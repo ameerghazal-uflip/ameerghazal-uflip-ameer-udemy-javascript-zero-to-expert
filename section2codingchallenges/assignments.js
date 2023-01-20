67f438b33f4371a9334d36befa6f5aa328524f0a
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-20 15:12:52 -0600

    lesson 12 resubmit

--- section2codingchallenges/assignments.js
+++ section2codingchallenges/assignments.js
@@ -1,9 +1,8 @@
-// Values and Variables 
+// Data Types
 
-let country = "United States";
-let continent = "North America"; // String
-let population = 331.45; // number
-
-console.log(country);
-console.log(continent);
-console.log(population);
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
