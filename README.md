e92bce9ff693dccb0628fdaf1daac314bb2c8ec5
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-20 15:08:46 -0600

    lesson 10 resubmit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ameer-udemy-javascript-zero-to-expert
+
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions)
+2. Log their values to the console
--- section2codingchallenges/assignments.js
+++ section2codingchallenges/assignments.js
@@ -1,16 +1,9 @@
-// let, const and var
+// Values and Variables 
 
+let country = "United States";
+let continent = "North America"; // String
+let population = 331.45; // number
 
-language = "English";
-const country = "United States";
-const continent = "North America";
-let population = 331.45;
-
-console.log(population / 2);
-console.log(population++);
-console.log(population > 6); // assuming we are in millons (units)
-console.log(population < 33);
-const description = country + " is in " + continent + ", and its " + population + " million people speak " + language;
-console.log(description);
-
-
+console.log(country);
+console.log(continent);
+console.log(population);
