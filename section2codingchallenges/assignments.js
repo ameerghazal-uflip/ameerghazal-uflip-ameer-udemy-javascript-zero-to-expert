ff94a4972411582b7aa76a5cca42049e5e4d83a7
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-20 13:18:21 -0600

    redo

---
+++ section2codingchallenges/assignments.js
@@ -0,0 +1,9 @@
+// Values and Variables 
+
+let country = "United States";
+let continent = "North America"; // String
+let population = 331.45; // number
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ section2codingchallenges/script.js
@@ -0,0 +1,16 @@
+
+let js = 'daily';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log("Whole lotta red");
+
+let firstName = "carti"; // let is used as a variable 
+console.log(firstName);
+console.log(firstName);
+console.log(firstName); // logs things to the computer using camelCase, similar to java
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Eater";
+
+console.log(myFirstJob);
