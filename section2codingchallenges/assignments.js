2d4e220189a965cf9644968ddd330acfcaab6b68
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-23 13:12:01 -0600

    update

--- section2codingchallenges/assignments.js
+++ section2codingchallenges/assignments.js
@@ -3,6 +3,6 @@
 
 // Data 1: Test for bill values 275, 40 and 430
 
-let bill = 40;
-bill >= 50 && bill <= 300 ? tip = (bill * 0.15) : tip = (bill * .20);
+const bill = 40;
+const tip = bill >= 50 && bill <= 300 ? tip = (bill * 0.15) : tip = (bill * .20);
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
