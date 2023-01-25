01a8f9e5acfe915d33795422cc93dbfc6ecf870c
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-25 17:56:14 -0600

    updated

--- JavaScript-Course-AmeerGhazal/assignments.js
+++ JavaScript-Course-AmeerGhazal/assignments.js
@@ -1,1 +1,8 @@
 // Looping Arrays, Breaking, and Continuing
+const populations = [331.45, 126.23, 200.23, 111];
+const percentages2 = [];
+
+for (let index = 0; index < populations.length; ++index) {
+
+}
+
