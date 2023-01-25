a6e03e80cf526c3de3cee17c85cabeb24eea6f74
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-25 14:49:06 -0600

    updated

--- JavaScript-Course-AmeerGhazal/assignments.js
+++ JavaScript-Course-AmeerGhazal/assignments.js
@@ -5,10 +5,11 @@ neighbors.push('Utopia'); // adds to the end=
 neighbors.pop(); /// removes last element
 console.log(neighbors);
 
-if (neighbors.includes('Germany')) {
+if (!(neighbors.includes('Germany'))) {
     console.log("Probably not a central country");
 }
 
 const value = neighbors.indexOf('Brazil');
 neighbors[value] = 'Filler';
 console.log(neighbors);
+
