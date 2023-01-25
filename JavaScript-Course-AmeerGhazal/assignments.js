64f893d8faeca892cd0e04f16dfd1f28fc2beb94
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-25 17:27:38 -0600

    fix

--- JavaScript-Course-AmeerGhazal/assignments.js
+++ JavaScript-Course-AmeerGhazal/assignments.js
@@ -1,1 +1,5 @@
-// Looping Arrays, Breaking, and Continuing
+// Iteration: The for Loop
+
+for (let votes = 1; votes <= 50; ++votes) {
+    console.log(`Voter number ${votes} is currently voting.`);
+}
