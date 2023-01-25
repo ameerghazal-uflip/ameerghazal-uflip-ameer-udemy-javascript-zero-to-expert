193ee57e0387016b6914b156b158c2bd0336eeb4
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-25 17:56:29 -0600

    udayte

--- JavaScript-Course-AmeerGhazal/assignments.js
+++ JavaScript-Course-AmeerGhazal/assignments.js
@@ -2,7 +2,13 @@
 const populations = [331.45, 126.23, 200.23, 111];
 const percentages2 = [];
 
-for (let index = 0; index < populations.length; ++index) {
-
+function percentageOfWorld1(population) {
+    return ((population / 7900) * 100);
 }
 
+
+for (let index = 0; index < populations.length; ++index) {
+    percentages2.push(percentageOfWorld1(populations[index]));
+}
+
+console.log(percentages2);
