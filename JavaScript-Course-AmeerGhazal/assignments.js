0526bf8d47c205bce6f7e30d598c5c6d04691066
Ameer Ghazal <ameerghazal@uflip.com>
2023-01-25 14:50:03 -0600

    challenge 2

--- JavaScript-Course-AmeerGhazal/assignments.js
+++ JavaScript-Course-AmeerGhazal/assignments.js
@@ -1,14 +1,17 @@
-// Basic Array Operations (Methods)
+// Coding Challenge 2 (Section 3)
 
-const neighbors = ['Brazil', 'Venezula', 'Argentina'];
-neighbors.push('Utopia'); // adds to the end=
-neighbors.pop(); /// removes last element
-console.log(neighbors);
+function calcTip(bill) {
+    if (bill >= 50 && bill <= 300) {
+        return (bill * 0.15); // this is the tip
+    }
 
-if (neighbors.includes('Germany')) {
-    console.log("Probably not a central country");
+    return (bill * 0.2);
 }
 
-const value = neighbors.indexOf('Brazil');
-neighbors[value] = 'Filler';
-console.log(neighbors);
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log(tips);
+
+
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(total);
