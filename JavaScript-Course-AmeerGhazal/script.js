eb3919c48d8ae9e5b9c8e10884ebddbb84f95085
Ameer Ghazal <ameerghazal@uflip.com>
2023-02-13 14:33:39 -0600

    3

--- JavaScript-Course-AmeerGhazal/script.js
+++ JavaScript-Course-AmeerGhazal/script.js
@@ -1,28 +1,8 @@
 'use strict';
 
-// Section 8 Lesson 99
+// Section 9 Lesson 103: Destructuring Arrays
+
 /*
-
-let age = 30;
-let oldAge = age;
-age = 31;
-
-console.log(age, oldAge);
-
-const me = {
-  name: 'Jonas',
-  age: 30,
-};
-
-const friend = me; // it is given the same reference, so it changes everything. In the stack, it points to the same address in the heap, meaning it changes everything at the address.
-friend.age = 27;
-
-console.log('Friend: ', friend);
-console.log('Me: ', me);
-
-*/
-
-// Section 8 Lesson 100
 
 let lastName = 'Williams';
 let oldLastName = lastName;
@@ -55,3 +35,5 @@ jessicaCopy.family.push('Mary');
 jessicaCopy.family.push('John'); // attempeting to manipulate an object within an object.
 console.log('Before marriage:', jessica2);
 console.log('After marriage:', jessicaCopy);
+
+*/
