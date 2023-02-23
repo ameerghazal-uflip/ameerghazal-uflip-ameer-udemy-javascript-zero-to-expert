7d4e84c4e864f8b10ea54ee1bb7cdb88499b3cd2
Ameer Ghazal <ameerghazal@uflip.com>
2023-02-23 11:55:43 -0600

    challenge in assignments

--- Assignments/assignments.js
+++ Assignments/assignments.js
@@ -1,47 +1,58 @@
-// Section 9 Coding Challenge 4
+// Section 10 Coding Challenge 1
+"use strict";
 
-document.body.append(document.createElement("textarea"));
-document.body.append(document.createElement("button"));
-const btn = document.querySelector("button");
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+  // 1.1 & 1.2
+  registerNewAnswer() {
+    let checker = false,
+      answer;
+    // checks the input
+    while (!checker) {
+      answer = Number(
+        prompt(
+          `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+        )
+      );
 
-btn.addEventListener("click", function () {
-  // makes the button functionable
-  const input = document.querySelector("textarea").value.split("\n"); // stores the userinput and splits it into an array for each newline.
+      if (typeof answer !== NaN && answer < 4 && answer >= 0) {
+        checker = true;
+      }
 
-  const camelCase = []; // array that will store the data.
-  for (const word of input) {
-    // loops through the array
-    if (!word.includes("_")) {
-      continue; // skips the word if it does not have an underscorer and moves the next iteration in the loop.
+      console.log(answer);
     }
+    //  typeof answer !== NaN || answer >= 4 || answer < 0) {
 
-    let copy = word.toLowerCase().replaceAll(" ", ""); // basically creates a copy.
-    const dash = copy.indexOf("_");
-    copy = copy.replace("_", ""); // gets rid of the space & underscorer.
+    let count = 0; // default
+    this.answers[answer] += ++count; // after the answer is given and checked, the index is the input number and it is added to that values count.
 
-    let slicer = copy.slice(dash);
-    slicer = slicer.replace(slicer[0], slicer[0].toUpperCase()); // slices at the correct index, and makes the first val a capital. Indexs the check before the print since it starts at 0. makes the index that was next to the underscorer a capital letter (we removed the "_" so staying at this index works.).
+    // 4. calls the method
+    this.displayResults();
+    this.displayResults("string");
+  },
 
-    const combined = copy.slice(0, dash) + slicer; // fully combined and working word
+  // 3. displays results to the user.
+  displayResults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are ${this.answers.join(", ")}.`); // use
+    }
+  },
+};
 
-    camelCase.push(
-      combined.padEnd(20) + " " + "✅".repeat(input.indexOf(word) + 1)
-    ); // adds the camel case version to the array. Also, it uses the repeat method to determine how many times it should print the check (it checks the indexOf the word in the larger array) and adds one for the starter. We pad to get the same format.
-  }
+poll.registerNewAnswer();
 
-  console.log(camelCase.join("\n"));
-});
+// 2. click selector
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll)); // binds correctly for the this keyword with that certain function.
 
-// JONAS SOLUTION
-/*
-for (const [i, row] of rows.entries()) {
-  const [first, second] = row.toLowerCase().trim().split('_');
+const tester = [5, 3, 2];
 
-  const output = `${first}${second.replace(
-    second[0],
-    second[0].toUpperCase()
-  )}`;
-  console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
-}
-
-*/
+// 5. Bonus, display the 2 arrays in test data w/o putting in the poll object.
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }); // uses default array type
+poll.displayResults.call({ answers: [...tester] }, "string"); // specifies the string param. after the this. keyword
--- README.md
+++ README.md
@@ -1,28 +1,41 @@
 # ameer-udemy-javascript-zero-to-expert
 
-// Section 9 Coding Challenge 4
+// Section 10 Coding Challenge 1
 
-Write a program that receives a list of variable names written in underscore_case, and convert them to camelCase.
-The input will come from a textarea inserted into the DOM (see code below to
-insert the elements), and conversion will happen when the button is pressed.
-Test data (pasted to textarea, including spaces):
-underscore_case
-first_name
-Some_Variable
-calculate_AGE
-delayed_departure
+A Closer Look at Functions
+Coding Challenge #1
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an
+array with the number of replies for each option. This data is stored in the starter
+'poll' object below.
+Your tasks:
 
-Should produce this output (5 separate console.log outputs):
-underscoreCase ✅
-firstName ✅✅
-someVariable ✅✅✅
-calculateAge ✅✅✅✅
-delayedDeparture ✅✅✅✅✅
-
-Hints:
-§ Remember which character defines a new line in the textarea �
-§ The solution only needs to work for a variable made out of 2 words, like a_b
-§ Start without worrying about the ✅. Tackle that only after you have the variable
-name conversion working �
-§ This challenge is difficult on purpose, so start watching the solution in case
-you're stuck. Then pause and continue!
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The
+   method does 2 things:
+   1.1. Display a prompt window for the user to input the number of the
+   selected option. The prompt should look like this:
+   What is your favourite programming language?
+   0: JavaScript
+   1: Python
+   2: Rust
+   3: C++
+   (Write option number)
+   1.2. Based on the input number, update the 'answers' array property. For
+   example, if the option is 3, increase the value at position 3 of the array by 1.
+1. Make sure to check if the input is a number and if the number makes
+   sense (e.g. answer 52 wouldn't make sense, right?)
+1. Call this method whenever the user clicks the "Answer poll" button.
+1. Create a method 'displayResults' which displays the poll results. The
+   method takes a string as an input (called 'type'), which can be either 'string'
+   or 'array'. If type is 'array', simply display the results array as it is, using
+   console.log(). This should be the default option. If type is 'string', display a
+   string like "Poll results are 13, 2, 4, 1".
+1. Run the 'displayResults' method at the end of each
+   'registerNewAnswer' method call.
+1. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+   data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+   object! So what should the this keyword look like in this situation?
+   The Complete JavaScript Course 21
+   Test data for bonus:
+   § Data 1: [5, 2, 3]
+   § Data 2: [1, 5, 3, 9, 6, 1]
