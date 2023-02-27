1c4d46e09c23167c2d6a6e9e655839fff5187e50
Ameer Ghazal <ameerghazal@uflip.com>
2023-02-27 16:14:39 -0600

    noyes

--- Notes/theory.js
+++ Notes/theory.js
@@ -971,6 +971,7 @@ For example: const totalDepositsUSD = movements
 - We can check current arr.
 - Do not overuse chaining. It can cause performace issues if the array is huge.
 - Also, it is a bad practice to chain a method that mutates an array.
+- Need to use the arr.length last param. effectivly. 
 
 
 Project-Bankist
