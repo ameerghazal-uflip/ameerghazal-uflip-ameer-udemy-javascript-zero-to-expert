c917d6cd5ec1f07984ab6b2d6c9525ca0270a273
Ameer Ghazal <ameerghazal@uflip.com>
2023-02-27 15:30:13 -0600

    challenge2 in assignments

--- Assignments/assignments.js
+++ Assignments/assignments.js
@@ -1,24 +1,20 @@
 // Section 11 Coding Challenge 1
 "use strict";
 
-// Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-//§ Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map((age) => (age <= 2 ? 2 * age : 16 + age * 4));
+  const filteredAges = humanAges.filter((age) => age >= 18);
 
-const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuliaRedo = dogsJulia.slice(1, -2); // returns a new copy from the 2nd index to the 2nd to last index exclusive
+  const average = filteredAges.reduce((accum, age) => accum + age, 0); // adds up all the values into one.
+  return average / filteredAges.length; // returns the average humanAges
 
-  const combinedData = dogsJuliaRedo.concat(dogsKate); // attaches both to an array.
+  // const avgEdited = filteredAges.reduce(
+  //   (accum, age, i, arr) => (accum + age) / arr.length,
+  //   0
+  // );
 
-  // loops over array for age
-  combinedData.forEach(function (age, index) {
-    // age is playing as the element in this forEach loop function (age, index, array), but we only need age
-    console.log(
-      `Dog number ${index + 1} is ${
-        age >= 3 ? `an adult, and is ${age} years old.` : `still a puppy`
-      }`
-    );
-  });
+  // return avgEdited;
 };
 
-checkDogs([3, 5, 2, 12, 17], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
