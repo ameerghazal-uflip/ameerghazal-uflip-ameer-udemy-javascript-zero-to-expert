86800510a2f54360f905b85b64be1e5e6b52e344
Ameer Ghazal <ameerghazal@uflip.com>
2023-04-28 15:11:12 -0500

    ..

---
+++ src/js/controller.js
@@ -0,0 +1,108 @@
+// Section 18 Lesson 305-306: Storing Bookmarks in local storage & Part 3 Planning.
+
+import * as model from './model.js'; // * imports everything exported from model.
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
+
+import icons from 'url:../img/icons.svg'; // parcel 2
+import 'core-js/stable'; // polyfilling.
+import 'regenerator-runtime/runtime';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1); // ur; hash
+    // console.log(id);
+
+    if (!id) return; //guard clause
+    recipeView.renderSpinner();
+
+    // 0) Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage()); // would also work with render
+
+    // 1) Updating bookmarks view
+    bookmarksView.update(model.state.bookmarks);
+
+    // 2) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 3) Rendering Recipe
+    recipeView.render(model.state.recipe);
+    // if we exported the class, we would do, const rec = new recipeView(model....)
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner(); // inherits form the View parent class
+
+    // 1) gets the search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) load search results
+    await model.loadSearchResults(query); // does not return anything but manipulate the state, so we do not have to store it
+
+    // 3) render results
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render inital pagination buttons
+    paginationView.render(model.state.search); // pass in the entire state object.
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // 1) Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage)); // state.search gets updated and from there that search is edited and used the for the next pagination call.
+
+  // 2) Render new pagination buttons
+  paginationView.render(model.state.search); // pass in the entire state object.
+};
+
+const controlServings = function (newServings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe); // more efficent.
+};
+
+const controlAddBookmark = function () {
+  // 1) Add or remove a bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  // if there is no bookmark, we add one
+  else model.deleteBookmark(model.state.recipe.id); // if there is one, we delete it
+
+  // 2) Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks); // goes into the list to display the bookmarks that are true.
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state?.bookmarks);
+};
+
+// loops over to add the eventListener
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
---
+++ src/js/model.js
@@ -0,0 +1,124 @@
+import { async } from 'regenerator-runtime';
+import { API_URL, RES_PER_PAGE } from './config.js';
+import { getJSON } from './helpers.js';
+
+export const state = {
+  recipe: {},
+  search: {
+    query: '',
+    results: [],
+    page: 1,
+    resultsPerPage: RES_PER_PAGE,
+  },
+  bookmarks: [],
+};
+
+export const loadRecipe = async function (id) {
+  try {
+    const data = await getJSON(`${API_URL}${id}`);
+
+    const { recipe } = data.data; // destructure for data on the different sides.
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    // deals with reloads, so it sets the bookmarked property if it is set.
+    if (state.bookmarks.some(bookmark => bookmark.id === id)) {
+      state.recipe.bookmarked = true;
+    } else state.recipe.bookmarked = false;
+
+    // console.log(state.recipe);
+  } catch (err) {
+    // Temp error handling
+    console.error(`${err} 💥💥💥💥`);
+    throw err; // rethrows the error
+  }
+};
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+    const data = await getJSON(`${API_URL}?search=${query}`);
+    console.log(data);
+
+    state.search.results = data.data.recipes.map(rec => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        publisher: rec.publisher,
+        image: rec.image_url,
+      };
+    });
+    state.search.page = 1; // resets the page when other things are searched for.
+
+    // console.log(state.search.results);
+  } catch (err) {
+    console.error(`${err} 💥💥💥💥`);
+    throw err; // rethrows the error for the controller.
+  }
+};
+
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage; // 0;
+  const end = page * state.search.resultsPerPage; // 9;
+
+  return state.search.results.slice(start, end); // returns the 10
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    // newQt = oldQt * newServings / old servings // 2 * 8 / 4 = 5
+  });
+
+  state.recipe.servings = newServings;
+};
+
+const persistBookmarks = function () {
+  localStorage.setItem('bookmarks', JSON.stringify(state.bookmarks));
+};
+
+export const addBookmark = function (recipe) {
+  // Add Bookmark into the array.
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmarked
+  if (recipe.id === state.recipe.id) {
+    state.recipe.bookmarked = true;
+  }
+
+  persistBookmarks();
+};
+
+export const deleteBookmark = function (id) {
+  // Delete bookmark
+  const index = state.bookmarks.findIndex(el => el.id === id);
+  state.bookmarks.splice(index, 1);
+
+  // Mark current recipe as NOT bookmarked
+  if (id === state.recipe.id) {
+    state.recipe.bookmarked = false;
+  }
+
+  persistBookmarks();
+};
+
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
+init();
+
+// to clear them all, by default it will be turned off
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
